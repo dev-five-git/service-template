8d0c6ab1f99199d1e98b2126dddf963e71431e18
owjs3901 <owjs3901@gmail.com>
2025-09-15 19:07:21 +0900

    Fix schema

--- apps/front/src/api.ts
+++ apps/front/src/api.ts
@@ -1,6 +1,6 @@
 import createClient from 'openapi-fetch'
 
-import type { paths } from '../../../schema'
+import type { paths } from '../schema'
 
 const client = createClient<paths>({ baseUrl: 'https://myapi.dev/v1/' })
 client.GET('/users/users', {

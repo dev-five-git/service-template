84ceed4ebeaa2b2c5c604f6d66f6257e05a9d43e
owjs3901 <owjs3901@gmail.com>
2025-11-26 16:00:53 +0900

    Fix main

--- apis/api/src/main.rs
+++ apis/api/src/main.rs
@@ -1,45 +1,38 @@
-//! Run with
-//!
-//! ```not_rust
-//! cargo run -p example-cors
-//! ```
+mod routes;
 
-use axum::{
-    Json, Router,
-    http::{HeaderValue, Method},
-    response::IntoResponse,
-    routing::get,
-};
 use std::{env, net::SocketAddr};
 use tower_http::cors::CorsLayer;
+use vespera::axum::http::{HeaderValue, Method};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(json)).layer(
-        // see https://docs.rs/tower-http/latest/tower_http/cors/index.html
-        // for more details
-        //
-        // pay attention that for some request types like posting content-type: application/json
-        // it is required to add ".allow_headers([http::header::CONTENT_TYPE])"
-        // or see this issue https://github.com/tokio-rs/axum/issues/849
-        CorsLayer::new()
-            .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
-            .allow_methods([Method::GET]),
-    );
+    // let config = Config::from_env();
     let port = env::var("PORT")
         .unwrap_or("8000".to_string())
         .parse::<u16>()
         .unwrap();
-    println!("API server is running on port {}", port);
-    serve(app, port).await;
-}
 
-async fn serve(app: Router, port: u16) {
+    // let db = create_db_connection(&config.database_url).await;
+
+    // let state = AppState { db, config };
+    let app = vespera::vespera!().layer(
+        CorsLayer::new()
+            .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+            .allow_methods([
+                Method::GET,
+                Method::POST,
+                Method::PUT,
+                Method::DELETE,
+                Method::OPTIONS,
+            ])
+            .allow_headers([
+                vespera::axum::http::header::CONTENT_TYPE,
+                vespera::axum::http::header::AUTHORIZATION,
+            ]),
+    );
+
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    println!("API server is running on port {}", port);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
-
-async fn json() -> impl IntoResponse {
-    Json(vec!["one", "two", "three"])
+    vespera::axum::serve(listener, app).await.unwrap();
 }

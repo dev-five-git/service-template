7d0bc83d6362dfbc20f62aeac8390ad64204cfd4
owjs3901 <owjs3901@gmail.com>
2025-12-16 23:41:51 +0900

    Update api

--- apis/api/src/main.rs
+++ apis/api/src/main.rs
@@ -1,35 +1,41 @@
 mod routes;
 
+use api::{
+    config::{Config, create_db_connection},
+    utils::AppState,
+};
 use std::{env, net::SocketAddr};
 use tower_http::cors::CorsLayer;
 use vespera::axum::http::{HeaderValue, Method};
 
 #[tokio::main]
 async fn main() {
-    // let config = Config::from_env();
+    let config = Config::from_env();
     let port = env::var("PORT")
         .unwrap_or("8000".to_string())
         .parse::<u16>()
         .unwrap();
 
-    // let db = create_db_connection(&config.database_url).await;
+    let db = create_db_connection(&config.database_url).await;
 
-    // let state = AppState { db, config };
-    let app = vespera::vespera!().layer(
-        CorsLayer::new()
-            .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
-            .allow_methods([
-                Method::GET,
-                Method::POST,
-                Method::PUT,
-                Method::DELETE,
-                Method::OPTIONS,
-            ])
-            .allow_headers([
-                vespera::axum::http::header::CONTENT_TYPE,
-                vespera::axum::http::header::AUTHORIZATION,
-            ]),
-    );
+    let state = AppState { db, config };
+    let app = vespera::vespera!(openapi = ["apps/front/openapi.json", "apps/admin/openapi.json"])
+        .with_state(state)
+        .layer(
+            CorsLayer::new()
+                .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+                .allow_methods([
+                    Method::GET,
+                    Method::POST,
+                    Method::PUT,
+                    Method::DELETE,
+                    Method::OPTIONS,
+                ])
+                .allow_headers([
+                    vespera::axum::http::header::CONTENT_TYPE,
+                    vespera::axum::http::header::AUTHORIZATION,
+                ]),
+        );
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     println!("API server is running on port {}", port);

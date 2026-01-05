f30507d31c42ef21a5fe5289d6ee5c65a9a9fd1b
owjs3901 <owjs3901@gmail.com>
2026-01-05 14:51:53 +0900

    Fix wrong code

--- apps/front/src/app/page.tsx
+++ apps/front/src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Box } from '@devup-ui/react'
+
 export default function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <Box bg="red" color="white">
+      HomePage
+    </Box>
+  )
 }
